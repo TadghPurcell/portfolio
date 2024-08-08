3ab807b6d303a522a38061b792804e83bdbd1947
Tadgh Purcell <tadghp@gmail.com>
2024-08-08 10:34:43 +0100

    Update skills section

---
+++ src/components/Skills.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import SectionHeading from './SectionHeading'
+import { skillsData } from '../lib/data'
+import { motion } from 'framer-motion'
+import { useSectionInView } from '../lib/hooks';
+import { Icon } from '@iconify/react';
+
+const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+};
+
+export default function Skills() {
+    const { ref } = useSectionInView('Skills');
+
+    return (
+        <section 
+            ref={ref} 
+            id='skills' 
+            className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'
+        >
+            <SectionHeading>My skills</SectionHeading>
+            <ul className='flex flex-wrap justify-center gap-2 text-lg text-gray-800'>
+                {
+                    skillsData.map((skill, index) => (
+                        <motion.li 
+                            className='flex flex-col items-center justify-center w-1/6 h-24'    
+                            key={index}
+                            variants={fadeInAnimationVariants}
+                            initial="initial"
+                            whileInView="animate"
+                            viewport={{
+                            once: true,
+                            }}
+                            custom={index}
+                        >
+                            <Icon icon={skill.iconUrl} className='text-6xl'/>
+                            {skill.name}
+                        </motion.li>
+                ))}
+            </ul>
+        </section>
+    )
+}
